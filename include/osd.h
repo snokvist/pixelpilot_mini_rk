b96a1c76c92f12f9f10483856764b8d0003b5ad9
Joakim <joakim.snokvist@gmail.com>
2025-09-28 19:47:39 +0200

    Expose OSD structure in public header

--- include/osd.h
+++ include/osd.h
@@ -4,10 +4,33 @@
 #include <stdint.h>
 
 #include "config.h"
+#include "drm_fb.h"
 #include "drm_modeset.h"
 #include "pipeline.h"
 
-typedef struct OSD OSD;
+typedef struct OSD {
+    int enabled;
+    int active;
+    uint32_t requested_plane_id;
+    uint32_t plane_id;
+    struct DumbFB fb;
+    int w;
+    int h;
+    int scale;
+    int refresh_ms;
+    uint32_t crtc_id;
+
+    uint32_t p_fb_id, p_crtc_id, p_crtc_x, p_crtc_y, p_crtc_w, p_crtc_h;
+    uint32_t p_src_x, p_src_y, p_src_w, p_src_h;
+    uint32_t p_zpos;
+    int have_zpos;
+    uint64_t zmin, zmax;
+    uint32_t p_alpha;
+    int have_alpha;
+    uint64_t alpha_min, alpha_max;
+    uint32_t p_blend;
+    int have_blend;
+} OSD;
 
 void osd_init(OSD *osd);
 int osd_setup(int fd, const AppCfg *cfg, const ModesetResult *ms, int video_plane_id, OSD *osd);
